5eb216e7ff99b445e3bf43a1723cd09bbbd26980
刘蘩 <liufan.dery@gmail.com>
2017-08-03 17:57:01 +0800

    numpy

--- Data_Process/Numpy/Numpy_Array.py
+++ Data_Process/Numpy/Numpy_Array.py
@@ -1,312 +1,398 @@
-'''
-1。创建数组
-2。保存数组
-3。操作数组
-
-
-'''
-# Numpy 的数组操作
-
-from numpy import *
-'''
-# 创建数组
-'''
-# v 与 M 对象都是 numpy 模块提供的 ndarray 类型
-v = array([1,2,3,4])
-m = array([[1,2],[3,4]])
-
-# v 与 M 数组的不同之处在于它们的维度。 我们可以通过 ndarray.shape 获得它的维度属性：
-# print(v.shape) # (4,)
-# print(m.shape) # (2, 2)
-
-# ndarray.size 数组的元素数量：
-# print(m.size) # 4
-
-# dtype 属性我们能获得数组元素的类型：
-# print(m.dtype)
-
-# 可以显示地定义元素类型通过在创建数组时使用 dtype 关键字参数：
-M = array([[1, 2], [3, 4]], dtype=complex)
-# array([[ 1.+0.j,  2.+0.j],
-#        [ 3.+0.j,  4.+0.j]])
-
-
-# 同样的，我们可以使用 numpy.shape 与 numpy.size 函数获取对应属性值：
-shape(m)
-size(m)
-
-'''
-ndarray 与 list差不多，但不用list但原因如下：
-Python 的 list 是动态类型，可以包含不同类型的元素，所以没有支持诸如点乘等数学函数，因为要为 list 实现这些操作会牺牲性能。
-Numpy 数组是 静态类型 并且 齐次。 元素类型在数组创建的时候就已经确定了。
-Numpy 数组节约内存。
-由于是静态类型，对其数学操作函数（如矩阵乘法，矩阵加法）的实现可以使用 C 或者 Fortran 完成。
-'''
-
-# create a range
-'''
-使用数组生成函数：
-当需要生产大数组时，手动创建显然是不明智的，我们可以使用函数来生成数组，最常用的有如下几个函数：
-'''
-# arange
-x_arange = arange(-1, 10, 1) # arguments: start, stop, step
-print(x_arange)
-# [-1  0  1  2  3  4  5  6  7  8  9]
-
-# linspace
-x_linspace = linspace(0,10,25)
-print(x_linspace)
-# [  0.           0.41666667   0.83333333   1.25         1.66666667
-#    2.08333333   2.5          2.91666667   3.33333333   3.75         4.16666667
-#    4.58333333   5.           5.41666667   5.83333333   6.25         6.66666667
-#    7.08333333   7.5          7.91666667   8.33333333   8.75         9.16666667
-#    9.58333333  10.        ]
-
-# logspace
-x_logspace = logspace(0,10,10,base=e)
-print(x_logspace)
-# [  1.00000000e+00   3.03773178e+00   9.22781435e+00   2.80316249e+01
-#    8.51525577e+01   2.58670631e+02   7.85771994e+02   2.38696456e+03
-   # 7.25095809e+03   2.20264658e+04]
-
-x,y = mgrid[0:5,0:5]
-print(x)
-# [[0 0 0 0 0]
-#  [1 1 1 1 1]
-#  [2 2 2 2 2]
-#  [3 3 3 3 3]
-#  [4 4 4 4 4]]
-print(y)
-# [[0 1 2 3 4]
-#  [0 1 2 3 4]
-#  [0 1 2 3 4]
-#  [0 1 2 3 4]
-#  [0 1 2 3 4]]
-
-x_rand = random.rand(5,5)
-x_randn = random.randn(5,5)
-
-x_diag = diag([1,2,3])
-print(x_diag)
-# [[1 0 0]
-#  [0 2 0]
-#  [0 0 3]]
-
-x_diag_k = diag([1,2,3],k=2)
-print(x_diag_k)
-# [[0 0 1 0 0]
-#  [0 0 0 2 0]
-#  [0 0 0 0 3]
-#  [0 0 0 0 0]
-#  [0 0 0 0 0]]
+# -*- coding: utf-8 -*-
 
-x_zeros = zeros((3,4))
-print(x_zeros)
-# [[ 0.  0.  0.  0.]
-#  [ 0.  0.  0.  0.]
-#  [ 0.  0.  0.  0.]]
+'''# 效率比较'''
+#-=-=--=-=-=-=
+'''# 效率比较'''
+# 向量相加-Python
+def pythonsum(n):
+    a = range(n)
+    b = range(n)
+    c = []
+    for i in range(len(a)):
+        a[i] = i ** 2
+        b[i] = i ** 3
+        c.append(a[i] + b[i])
+    return c
 
-x_ones = ones((3,4))
-print(x_ones)
-# [[ 1.  1.  1.  1.]
-#  [ 1.  1.  1.  1.]
-#  [ 1.  1.  1.  1.]]
 
-'''
-保存数组：
-'''
-# savetxt
-savetxt("random-matrix.csv", m, fmt='%.5f')
+# 向量相加-NumPy
+import numpy as np
 
-# 使用 numpy.save 与 numpy.load 保存和读取原生文件类型：
-x_save = save("random-matrix.npy", m)
-x_load = load("random-matrix.npy")
 
-# nnumpy数组的常用属性
-x_itemsize = M.itemsize # bytes per element
-x_nbytes = M.nbytes # number of bytes
-x_ndim = M.ndim # number of dimensions
+def numpysum(n):
+    a = np.arange(n) ** 2
+    b = np.arange(n) ** 3
+    c = a + b
+    return c
 
+import sys
+from datetime import datetime
+import numpy as np
 
-'''
-操作数组：
-'''
-# 索引
+size = 1000
 
-# 最基本的，我们用方括号进行检索：
-# v is a vector, and has only one dimension, taking one index
-'''
-# v[0]
-'''
-# M is a matrix, or a 2 dimensional array, taking two indices
-'''
-# M[1,1]
-'''
-# 如果是N(N > 1)维数列，而我们在检索时省略了一个索引值则会返回一整行((N-1)维数列)：
-# M
-# => array([[ 0.70506801,  0.54618952,  0.31039856],
-#           [ 0.26640475,  0.10358152,  0.73231132],
-#           [ 0.07987128,  0.34462854,  0.91114433]])
-'''
-# M[1]
-'''
-# => array([ 0.26640475,  0.10358152,  0.73231132])
+start = datetime.now()
+c = pythonsum(size)
+delta = datetime.now() - start
+print("The last 2 elements of the sum", c[-2:])
+print("PythonSum elapsed time in microseconds", delta.microseconds)
 
+start = datetime.now()
+c = numpysum(size)
+delta = datetime.now() - start
+print("The last 2 elements of the sum", c[-2:])
+print("NumPySum elapsed time in microseconds", delta.microseconds)
 
-"使用':'能达到同样的效果:"
-# M[1,:] # row 1
-# => array([ 0.26640475,  0.10358152,  0.73231132])
+'''# 效率比较'''
+#-=-=--=-=-=-=
+'''# 效率比较'''
 
-# M[:,1] # column 1
-# => array([ 0.54618952,  0.10358152,  0.34462854])
 
-"可以利用索引进行赋值："
-# M[0,0] = 1
+# numpy数组
+a = np.arange(5)
+a.dtype
 
-# => array([[ 1.        ,  0.54618952,  0.31039856],
-#           [ 0.26640475,  0.10358152,  0.73231132],
-#           [ 0.07987128,  0.34462854,  0.91114433]])
+a
+a.shape
 
+# 创建多维数组
+m = np.array([np.arange(2), np.arange(2)])
 
-# also works for rows and columns
-# M[1,:] = 0
-# M[:,2] = -1
+print(m)
 
-# => array([[ 1.        ,  0.54618952, -1.        ],
-#           [ 0.        ,  0.        , -1.        ],
-#           [ 0.07987128,  0.34462854, -1.        ]])
+print(m.shape)
 
+print(m.dtype)
 
+np.zeros(10)
+np.zeros((3, 6))
+np.empty((2, 3, 2))
+np.arange(15)
 
-'''切片索引'''
+# 选取数组元素
+a = np.array([[1, 2], [3, 4]])
 
-# 切片索引语法：M[lower:upper:step]
+print("In: a")
+print(a)
 
-# A = array([1,2,3,4,5])
+print("In: a[0,0]")
+print(a[0, 0])
 
-# => array([1, 2, 3, 4, 5])
+print("In: a[0,1]")
+print(a[0, 1])
 
-# A[1:3]
+print("In: a[1,0]")
+print(a[1, 0])
 
-# => array([2, 3])
+print("In: a[1,1]")
+print(a[1, 1])
 
+# numpy数据类型
+print("In: float64(42)")
+print(np.float64(42))
 
-# 进行切片赋值时，原数组会被修改：
+print("In: int8(42.0)")
+print(np.int8(42.0))
 
-# A[1:3] = [-2,-3]
+print("In: bool(42)")
+print(np.bool(42))
 
-# => array([ 1, -2, -3,  4,  5])
+print(np.bool(0))
 
+print("In: bool(42.0)")
+print(np.bool(42.0))
 
-# 我们可以省略 M[lower:upper:step] 中的任意参数:
+print("In: float(True)")
+print(np.float(True))
+print(np.float(False))
 
-# A[::] # lower, upper, step all take the default values
+print("In: arange(7, dtype=uint16)")
+print(np.arange(7, dtype=np.uint16))
 
-# => array([ 1, -2, -3,  4,  5])
+print("In: int(42.0 + 1.j)")
+try:
+    print(np.int(42.0 + 1.j))
+except TypeError:
+    print("TypeError")
+    # Type error
 
-# A[::2] # step is 2, lower and upper defaults to the beginning and end of the array
+    print("In: float(42.0 + 1.j)")
+    print(float(42.0 + 1.j))
+    # Type error
 
-# => array([ 1, -3,  5])
+    # 数据类型转换
+    arr = np.array([1, 2, 3, 4, 5])
+    arr.dtype
+    float_arr = arr.astype(np.float64)
+    float_arr.dtype
 
-# A[:3] # first three elements
+    arr = np.array([3.7, -1.2, -2.6, 0.5, 12.9, 10.1])
+    arr
+    arr.astype(np.int32)
 
-# => array([ 1, -2, -3])
+    numeric_strings = np.array(['1.25', '-9.6', '42'], dtype=np.string_)
+    numeric_strings.astype(float)
 
-# A[3:] # elements from index 3
+    # 数据类型对象
+    a = np.array([[1, 2], [3, 4]])
 
-# => array([4, 5])
+    print(a.dtype.byteorder)
 
+    print(a.dtype.itemsize)
 
-'''负值索引从数组尾开始计算：'''
+    # 字符编码
+    print(np.arange(7, dtype='f'))
+    print(np.arange(7, dtype='D'))
 
-# A = array([1,2,3,4,5])
-# A[-1] # the last element in the array
+    print(np.dtype(float))
 
-# => 5
+    print(np.dtype('f'))
 
-# A[-3:] # the last three elements
+    print(np.dtype('d'))
 
-# => array([3, 4, 5])
+    print(np.dtype('f8'))
 
+    print(np.dtype('Float64'))
 
-'''索引切片在多维数组的应用也是一样的:'''
+    # dtype类的属性
+    t = np.dtype('Float64')
 
-A = array([[n+m*10 for n in range(5)] for m in range(5)])
+    print(t.char)
 
-# => array([[ 0,  1,  2,  3,  4],
-#           [10, 11, 12, 13, 14],
-#           [20, 21, 22, 23, 24],
-#           [30, 31, 32, 33, 34],
-#           [40, 41, 42, 43, 44]])
+    print(t.type)
 
+    print(t.str)
 
-# a block from the original array
-# A[1:4, 1:4]
+    # 创建自定义数据类型
+    t = np.dtype([('name', np.str_, 40), ('numitems', np.int32), ('price', np.float32)])
+    print(t)
 
-# => array([[11, 12, 13],
-#           [21, 22, 23],
-#           [31, 32, 33]])
+    print(t['name'])
 
-# strides
-# A[::2, ::2]
+    itemz = np.array([('Meaning of life DVD', 42, 3.14), ('Butter', 13, 2.72)], dtype=t)
 
-# => array([[ 0,  2,  4],
-#           [20, 22, 24],
-#           [40, 42, 44]])
+    print(itemz[1])
 
+    # 数组与标量的运算
+    arr = np.array([[1., 2., 3.], [4., 5., 6.]])
+    arr
+    arr * arr
+    arr - arr
 
-'''
-高级索引（Fancy indexing）
-'''
+    1 / arr
+    arr ** 0.5
 
-# 指使用列表或者数组进行索引:
+    # 一维数组的索引与切片
+    a = np.arange(9)
 
-# row_indices = [1, 2, 3]
-# A[row_indices]
+    print(a[3:7])
 
-# => array([[10, 11, 12, 13, 14],
-#           [20, 21, 22, 23, 24],
-#           [30, 31, 32, 33, 34]])
+    print(a[:7:2])
 
-# col_indices = [1, 2, -1] # remember, index -1 means the last element
-# A[row_indices, col_indices]
+    print(a[::-1])
 
-# => array([11, 22, 34])
+    s = slice(3, 7, 2)
+    print(a[s])
 
-'''
-我们也可以使用索引掩码:
-'''
-# B = array([n for n in range(5)])
+    s = slice(None, None, -1)
 
-# => array([0, 1, 2, 3, 4])
+    print(a[s])
 
-# row_mask = array([True, False, True, False, False])
-# B[row_mask]
+    # 多维数组的切片与索引
+    b = np.arange(24).reshape(2, 3, 4)
 
-# => array([0, 2])
+    print(b.shape)
 
-# same thing
-# row_mask = array([1,0,1,0,0], dtype=bool)
-# B[row_mask]
+    print(b)
 
-# => array([0, 2])
+    print(b[0, 0, 0])
 
-'''
-使用比较操作符生成掩码:
-'''
-x = arange(0, 10, 0.5)
+    print(b[:, 0, 0])
 
-# => array([ 0. ,  0.5,  1. ,  1.5,  2. ,  2.5,  3. ,  3.5,  4. ,  4.5,  5. ,
-#            5.5,  6. ,  6.5,  7. ,  7.5,  8. ,  8.5,  9. ,  9.5])
+    print(b[0])
 
-# mask = (5 < x) * (x < 7.5)
+    print(b[0, :, :])
 
-# => array([False, False, False, False, False, False, False, False, False,
-#           False, False,  True,  True,  True,  True, False, False, False,
-#           False, False], dtype=bool)
+    print(b[0, ...])
 
-# x[mask]
+    print(b[0, 1])
 
-# => array([ 5.5,  6. ,  6.5,  7. ])
+    print(b[0, 1, ::2])
 
+    print(b[..., 1])
+
+    print(b[:, 1])
+
+    print(b[0, :, 1])
+
+    print(b[0, :, -1])
+
+    print(b[0, ::-1, -1])
+
+    print(b[0, ::2, -1])
+
+    print(b[::-1])
+
+    s = slice(None, None, -1)
+    print(b[(s, s, s)])
+
+    # 布尔型索引
+    names = np.array(['Bob', 'Joe', 'Will', 'Bob', 'Will', 'Joe', 'Joe'])
+    data = randn(7, 4)
+    names
+    data
+
+    names == 'Bob'
+    data[names == 'Bob']
+
+    data[names == 'Bob', 2:]
+    data[names == 'Bob', 3]
+
+    names != 'Bob'
+    data[-(names == 'Bob')]
+
+    mask = (names == 'Bob') | (names == 'Will')
+    mask
+    data[mask]
+
+    data[data < 0] = 0
+    data
+
+    data[names != 'Joe'] = 7
+    data
+
+    # 花式索引
+    arr = np.empty((8, 4))
+    for i in range(8):
+        arr[i] = i
+    arr
+
+    arr[[4, 3, 0, 6]]
+
+    arr[[-3, -5, -7]]
+
+    arr = np.arange(32).reshape((8, 4))
+    arr
+    arr[[1, 5, 7, 2], [0, 3, 1, 2]]
+
+    arr[[1, 5, 7, 2]][:, [0, 3, 1, 2]]
+
+    arr[np.ix_([1, 5, 7, 2], [0, 3, 1, 2])]
+
+    # 数组转置
+    arr = np.arange(15).reshape((3, 5))
+    arr
+    arr.T
+
+    # 改变数组的维度
+    b = np.arange(24).reshape(2, 3, 4)
+
+    print(b)
+
+    print(b.ravel())
+
+    print(b.flatten())
+
+    b.shape = (6, 4)
+
+    print(b)
+
+    print(b.transpose())
+
+    b.resize((2, 12))
+
+    print(b)
+
+    # 组合数组
+    a = np.arange(9).reshape(3, 3)
+
+    print(a)
+
+    b = 2 * a
+
+    print(b)
+
+    print(np.hstack((a, b)))
+
+    print(np.concatenate((a, b), axis=1))
+
+    print(np.vstack((a, b)))
+
+    print(np.concatenate((a, b), axis=0))
+
+    print(np.dstack((a, b)))
+
+    oned = np.arange(2)
+
+    print(oned)
+
+    twice_oned = 2 * oned
+
+    print(twice_oned)
+
+    print(np.column_stack((oned, twice_oned)))
+
+    print(np.column_stack((a, b)))
+
+    print(np.column_stack((a, b)) == np.hstack((a, b)))
+
+    print(np.row_stack((oned, twice_oned)))
+
+    print(np.row_stack((a, b)))
+
+    print(np.row_stack((a, b)) == np.vstack((a, b)))
+
+    # 数组的分割
+    a = np.arange(9).reshape(3, 3)
+
+    print(a)
+
+    print(np.hsplit(a, 3))
+
+    print(np.split(a, 3, axis=1))
+
+    print(np.vsplit(a, 3))
+
+    print(np.split(a, 3, axis=0))
+
+    c = np.arange(27).reshape(3, 3, 3)
+
+    print(c)
+
+    print(np.dsplit(c, 3))
+
+    # 数组的属性
+    b = np.arange(24).reshape(2, 12)
+    b.ndim
+    b.size
+    b.itemsize
+    b.nbytes
+
+    b = np.array([1. + 1.j, 3. + 2.j])
+    b.real
+    b.imag
+
+    b = np.arange(4).reshape(2, 2)
+    b.flat
+    b.flat[2]
+
+    # 数组的转换
+    b = np.array([1. + 1.j, 3. + 2.j])
+    print(b)
+
+    print(b.tolist())
+
+    print(b.tostring())
+
+    print(np.fromstring(
+        '\x00\x00\x00\x00\x00\x00\xf0?\x00\x00\x00\x00\x00\x00\xf0?\x00\x00\x00\x00\x00\x00\x08@\x00\x00\x00\x00\x00\x00\x00@',
+        dtype=complex))
+
+    print(np.fromstring('20:42:52', sep=':', dtype=int))
+
+    print(b)
+
+    print(b.astype(int))
+
+    print(b.astype('complex'))
 
---
+++ Data_Process/Pandas/ldjdldlkc.sa.py
@@ -0,0 +1,6 @@
+from pandas import Series,DataFrame
+import pandas as pd
+
+obj = Series([4,3,-9,5],index=['s','a','q','p'])
+print(obj)
+
